b8e3a584576d9de105f1e55f65cdaa85dd2c9906
devkiptoo <114746793+devkiptoo@users.noreply.github.com>
2022-10-24 16:09:54 +0300

    Update cards.js
    fixed a typo

--- cards.js
+++ cards.js
@@ -1,7 +1,6 @@
 let count = 0;
 
 function cc(card) {
-  // Only change code below this line
   switch (card) {
     case 2:
     case 3:
@@ -23,7 +22,6 @@ function cc(card) {
   } else {
     return count + " Hold";
   }
-  // Only change code above this line
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
